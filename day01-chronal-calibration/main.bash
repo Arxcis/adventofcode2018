00cb09b8ec508951f066978d11cad3cc9b422932
tholok97 <thomas.lokkeborg@mimer.no>
2018-12-01 19:08:34 +0100

    WIP solution to day 1 part 2 - solves example inputs, and is currently cracking on the real input - commited for reference. will change if turns out to be wrong

--- day01-chronal-calibration/main.bash
+++ day01-chronal-calibration/main.bash
@@ -11,3 +11,52 @@ for var in $INPUT; do
 done
 
 echo "part1 $RESULT"
+
+# PART 2
+
+SEEN=()
+CURRENT=0
+INPUT=("$@")
+INPUT_LENGTH=${#INPUT[@]}
+INPUT_INDEX=0
+DONE=false
+
+while true; do
+
+
+    # if already seen, break. we're done
+    for E in "${SEEN[@]}"; do
+        if [[ $CURRENT = $E ]]; then
+            echo "yay!"
+            echo $E
+            DONE=true
+            break
+        fi
+    done
+
+    if [[ $DONE = true ]]; then
+        break
+    fi
+
+    # if got here, not already seen. add to seen
+    SEEN+=($CURRENT)
+
+
+
+    # calc next freq
+    CHANGE="${INPUT[$INPUT_INDEX]}"
+    CURRENT=$((CURRENT + CHANGE))
+
+    #echo "CURR: $CURRENT, I: $INPUT_INDEX, CHANGE (made): $CHANGE"
+
+    # make sure the loop wraps
+    ((INPUT_INDEX++))
+    if [[ $INPUT_INDEX -ge $INPUT_LENGTH ]]; then
+        INPUT_INDEX=0
+        #echo "looping... size of seen: ${#SEEN[@]}"
+    fi
+
+done
+
+echo "part2 $CURRENT"
+
