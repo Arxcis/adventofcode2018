7f19ab6158f49afacf80e2b1f3f96546bcc16fb4
Jonas Johan solsvik <jonasjso@protonmail.com>
2018-12-01 22:45:16 +0100

    day01/main.cpp - converted from file to cmdargs

--- day01-chronal-calibration/main.cpp
+++ day01-chronal-calibration/main.cpp
@@ -1,58 +1,49 @@
 #include <iostream>
-#include <fstream>
 #include <string>
 #include <map>
 #include <vector>
 
-int main()
+int main(int argc, char** argv)
 {
-    std::ifstream fileInput;
-    fileInput.open("input");
+    if (argc < 2) {
+        std::cerr << "Missing input\n";
+        return 1;
+    }
 
-    if (fileInput.is_open())
+    std::string line;
+    std::vector<int> input;
+    for (int i = 1; i < argc; i++) {
+        line = argv[i];
+        input.push_back(atoi(line.c_str()));
+    }
+
+    // Calculates puzzle 1
+    int total = 0;
+    for (auto var : input)
     {
-        std::string line;
-        std::vector<int> input;
+        total += var;
+    }
 
-        // Read contents of file and stores it in a vector
-        while (std::getline(fileInput, line))
-        {
-            input.push_back(atoi(line.c_str()));
-        }
-        fileInput.close();
+    std::cout << "part1 " << total << '\n';
 
 
-        // Calculates puzzle 1
-        int total = 0;
+    // Calculates puzzle 2;
+    std::map<int, int> duplicates;
+    int current = 0;
+    bool found = false;
+    while (!found)
+    {
         for (auto var : input)
         {
-            total += var;
-        }
-
-        std::cout << "part1 " << total << '\n';
-
-
-        // Calculates puzzle 2;
-        std::map<int, int> duplicates;
-        int current = 0;
-        bool found = false;
-        while (!found)
-        {
-            for (auto var : input)
+            current += var;
+            auto ret = duplicates.insert(std::pair<int, int>(current, 1));
+            if (ret.second == false)
             {
-                current += var;
-                auto ret = duplicates.insert(std::pair<int, int>(current, 1));
-                if (ret.second == false)
-                {
-                    std::cout << "part2 " << current << '\n';
-                    found = true;
-                    break;
-                }
+                std::cout << "part2 " << current << '\n';
+                found = true;
+                break;
             }
         }
     }
-
-    else std::cout << "Unable to open file!";
-
     return 0;
 }
