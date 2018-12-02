0b4768b9fa727f986a502f31cfecd427090a4d76
Jonas Johan solsvik <jonasjso@protonmail.com>
2018-12-02 22:49:05 +0100

    test - using cat to pass input to args

--- test-util.js
+++ test-util.js
@@ -4,29 +4,19 @@ const _exec = require('child_process').exec;
 
 exports.testGo = async (t, dirpath) => {
 
-    let input,expected
-    try {
-        expected = await readFile(`${dirpath}/output`)
-        input = await readFile(`${dirpath}/input`)
-                      .then(input => input.split('\n').join(' '))
-    } catch (e) {t.fail(e)}
+    try { var expected = await readFile(`${dirpath}/output`) } catch (e) { t.fail(e) }
 
-    let output = await exec(`go run ${dirpath}/main.go ${input}`)
+    let output = await exec(`go run ${dirpath}/main.go $(cat ${dirpath}/input)`)
     
     t.is(output, expected)
 }
 
 exports.testCpp = async (t, dirpath) => {
 
-    let input,expected
-    try {
-        expected = await readFile(`${dirpath}/output`)
-        input = await readFile(`${dirpath}/input`)
-                      .then(input => input.split('\n').join(' '))
-    } catch (e) {t.fail(e)}
+    try { var expected = await readFile(`${dirpath}/output`) } catch (e) { t.fail(e) }
 
     await exec(`g++ -std=c++17 ${dirpath}/main.cpp -o main-cpp`)
-    let output = await exec(`./main-cpp ${input}`)
+    let output = await exec(`./main-cpp $(cat ${dirpath}/input)`)
     await exec(`rm main-cpp`)
 
     t.is(output, expected)
@@ -34,43 +24,28 @@ exports.testCpp = async (t, dirpath) => {
 
 exports.testBash = async (t, dirpath) => {
 
-    let input,expected
-    try {
-        expected = await readFile(`${dirpath}/output`)
-        input = await readFile(`${dirpath}/input`)
-                      .then(input => input.split('\n').join(' '))
-    } catch (e) {t.fail(e)}
+    try { var expected = await readFile(`${dirpath}/output`) } catch (e) { t.fail(e) }
 
-    let output = await exec(`bash  ${dirpath}/main.bash ${input}`);
+    let output = await exec(`bash ${dirpath}/main.bash $(cat ${dirpath}/input)`);
     
     t.is(output, expected)
 }
 
 exports.testPython = async (t, dirpath) => {
 
-    let input,expected
-    try {
-        expected = await readFile(`${dirpath}/output`)
-        input = await readFile(`${dirpath}/input`)
-                      .then(input => input.split('\n').join(' '))
-    } catch (e) {t.fail(e)}
+    try { var expected = await readFile(`${dirpath}/output`) } catch (e) { t.fail(e) }
 
-    let output = await exec(`python3 ${dirpath}/main.py ${input}`);
+    let output = await exec(`python3 ${dirpath}/main.py $(cat ${dirpath}/input)`);
     
     t.is(output, expected)
 }
 
 exports.testRust = async (t, dirpath) => {
 
-    let input,expected
-    try {
-        expected = await readFile(`${dirpath}/output`)
-        input = await readFile(`${dirpath}/input`)
-                      .then(input => input.split('\n').join(' '))
-    } catch (e) {t.fail(e)}
+    try { var expected = await readFile(`${dirpath}/output`) } catch (e) { t.fail(e) }
 
     await exec(`rustc ${dirpath}/main.rs -o main-rs`);
-    let output = await exec(`./main-rs ${input}`)
+    let output = await exec(`./main-rs  $(cat ${dirpath}/input)`)
     await exec(`rm main-rs`)
 
     t.is(output, expected)
