6a7688d55f9290d2def762c590694bfebf4e4189
Jonas Johan solsvik <jonasjso@protonmail.com>
2018-12-02 22:13:10 +0100

    test - add skip tests for unimplemented solutions

--- day01-chronal-calibration/test.js
+++ day01-chronal-calibration/test.js
@@ -10,4 +10,5 @@ const dirpath = __dirname
 test('main.go', async t => await testGo(t, dirpath))
 test('main.cpp', async t => await testCpp(t, dirpath))
 test('main.bash', async t => await testBash(t, dirpath))
-//test('main.rs', async t => await testRust(t, dirpath))
+test.skip('main.py', async t => await testPython(t, dirpath))
+test.skip('main.rs', async t => await testRust(t, dirpath))
--- day02-inventory-management-system/test.js
+++ day02-inventory-management-system/test.js
@@ -4,8 +4,12 @@ import {
     testCpp,
     testBash,
     testPython,
+    testRust,
 } from '../test-util'
 
 const dirpath = __dirname
-test('main.go', async t => await testGo(t, dirpath))
 test('main.py', async t => await testPython(t, dirpath))
+test.skip('main.go', async t => await testGo(t, dirpath))
+test.skip('main.cpp', async t => await testCpp(t, dirpath))
+test.skip('main.bash', async t => await testBash(t, dirpath))
+test.skip('main.rs', async t => await testRust(t, dirpath))
