ce7fa79f214e913e939e286cb05ac9e9836731a1
Jonas Johan solsvik <jonasjso@protonmail.com>
2018-12-02 03:18:10 +0100

    test - removed shelljs dependency
    - using child-process instead. It is async
    so it is even better

--- test-util.js
+++ test-util.js
@@ -1,16 +1,14 @@
-const fs = require('fs')
 const test = require('ava')
-const shell = require('shelljs')
 
 exports.testGo = dirpath => {
-    test('main.go', async t => {
+    test(`main.go`, async t => {
 
         let output = await readFile(dirpath+"/output")
         let input = await readFile(dirpath+"/input")
 
         input = input.split('\n').join(' ')
 
-        let result = shell.exec(`go run ${dirpath}/main.go ${input}`, {silent: true}).stdout
+        let result = await exec(`go run ${dirpath}/main.go ${input}`)
         if (result !== output) {
             t.fail(`${result} !== \n${output}`)
         }
@@ -27,9 +25,9 @@ exports.testCpp = dirpath => {
 
         input = input.split('\n').join(' ')
 
-        shell.exec(`g++ -std=c++17 ${dirpath}/main.cpp -o ${dirpath}/main`, {silent: true})
-        let result = shell.exec(`${dirpath}/main ${input}`, {silent: true}).stdout
-        shell.rm(`${dirpath}/main`)
+        await exec(`g++ -std=c++17 ${dirpath}/main.cpp -o ${dirpath}/main`)
+        let result = await exec(`${dirpath}/main ${input}`, {silent: true})
+        await exec(`rm ${dirpath}/main`)
 
         if (result !== output) {
             t.fail(`${result} !== \n${output}`)
@@ -47,7 +45,8 @@ exports.testBash = dirpath => {
 
         input = input.split('\n').join(' ')
 
-        let result = shell.exec(`bash  ${dirpath}/main.bash ${input}`, {silent: true}).stdout
+        let result = await exec(`bash  ${dirpath}/main.bash ${input}`);
+
         if (result !== output) {
             t.fail(`${result} !== \n${output}`)
         }
@@ -57,6 +56,16 @@ exports.testBash = dirpath => {
 }
 
 
+const _exec = require('child_process').exec;
+const exec = (command) => {
+    return new Promise((resolve) => {
+        _exec(command, (err, stdout) => {
+            resolve(stdout)
+        });
+    })
+}
+
+const fs = require('fs')
 const readFile = (dirpath) => {
     return new Promise((resolve, reject) => {
         fs.readFile(dirpath, function (err, data) {
