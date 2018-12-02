c10595764fdbb1d7cc0201a5a54ba1b65e8ffacf
Jonas Johan solsvik <jonasjso@protonmail.com>
2018-12-02 20:08:47 +0100

    merge Celebrian/master day02-golang

--- day02-inventory-management-system/test.js
+++ day02-inventory-management-system/test.js
@@ -7,7 +7,5 @@ import {
 } from '../test-util'
 
 const dirpath = __dirname
-//test('main.go', async t => await testGo(t, dirpath))
-//test('main.cpp', async t => await testCpp(t, dirpath))
-//test('main.bash', async t => await testBash(t, dirpath))
+test('main.go', async t => await testGo(t, dirpath))
 test('main.py', async t => await testPython(t, dirpath))
