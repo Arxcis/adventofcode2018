ddeb9de3e09f334de0f9390c4d35951043457576
Jonas Johan solsvik <jonasjso@protonmail.com>
2018-12-02 14:58:10 +0100

    test - add testPython

--- day02-inventory-management-system/test.js
+++ day02-inventory-management-system/test.js
@@ -3,9 +3,11 @@ import {
     testGo,
     testCpp,
     testBash,
+    testPython,
 } from '../test-util'
 
 const dirpath = __dirname
 //test('main.go', async t => await testGo(t, dirpath))
 //test('main.cpp', async t => await testCpp(t, dirpath))
 //test('main.bash', async t => await testBash(t, dirpath))
+test('main.py', async t => await testPython(t, dirpath))
