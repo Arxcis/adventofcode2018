2ceeb3768091e094481d234287dd94ac7c7d8df8
Jonas Johan solsvik <jonasjso@protonmail.com>
2018-12-02 11:37:51 +0100

    test - remove redundant backticks

--- day01-chronal-calibration/test.js
+++ day01-chronal-calibration/test.js
@@ -6,6 +6,6 @@ import {
 } from '../test-util'
 
 const dirpath = __dirname
-test(`main.go`, async t => await testGo(t, dirpath))
-test(`main.cpp`, async t => await testCpp(t, dirpath))
-test(`main.bash`, async t => await testBash(t, dirpath))
+test('main.go', async t => await testGo(t, dirpath))
+test('main.cpp', async t => await testCpp(t, dirpath))
+test('main.bash', async t => await testBash(t, dirpath))
