25bd0eceedd8ec036fbfb83226825579dfaa59a3
tholok97 <thomas.lokkeborg@mimer.no>
2018-12-03 17:23:01 +0100

    solution to day 3 part 1 in Bash (slow) - solves example problem - estimated to use 2 hours on the real problem..

--- day03-no-matter-how-you-slice-it/main.bash
+++ day03-no-matter-how-you-slice-it/main.bash
@@ -12,46 +12,70 @@ for each pair i,j in 0..WIDTH,0..HEIGHT
 
 
 '
-CLAIMS="$@"
+CLAIMS_RAW="$@"
+declare -A CLAIMS_MAP
+
+# build claim datastructure
+# ( x y w h )
+# (using fake assoc array https://stackoverflow.com/questions/11233825/multi-dimensional-arrays-in-bash)
+
+CLAIMS_COUNTER=-1
+while read -r CLAIM; do
+
+    ((CLAIMS_COUNTER++))
+
+    # don't question the awk..... it works...
+    # source https://kuther.net/howtos/howto-print-text-between-tags-or-characters-awk-or-sed
+    CLAIM_X=$(echo $CLAIM | awk -F'[@ |,]' '{print $4}')
+    CLAIM_Y=$(echo $CLAIM | awk -F'[,|:]' '{print $2}')
+    CLAIM_W=$(echo $CLAIM | awk -F'[ |x]' '{print $4}')
+    CLAIM_H=$(echo $CLAIM | awk -F'[x|$]' '{print $2}')
+
+
+    CLAIMS_MAP[$CLAIMS_COUNTER,x]=$CLAIM_X
+    CLAIMS_MAP[$CLAIMS_COUNTER,y]=$CLAIM_Y
+    CLAIMS_MAP[$CLAIMS_COUNTER,w]=$CLAIM_W
+    CLAIMS_MAP[$CLAIMS_COUNTER,h]=$CLAIM_H
+
+    echo "$CLAIM $CLAIMS_COUNTER --> ${CLAIMS_MAP[$CLAIMS_COUNTER,x]} ${CLAIMS_MAP[$CLAIMS_COUNTER,y]} ${CLAIMS_MAP[$CLAIMS_COUNTER,w]} ${CLAIMS_MAP[$CLAIMS_COUNTER,h]}"
+
+done <<< "$CLAIMS_RAW"
 
 
 
-WIDTH=20
+NUM_CLAIMS=$CLAIMS_COUNTER
+WIDTH=1000
 HEIGHT=$WIDTH
 OVERLAP_COUNT=0
 
 for (( i=0; i<$WIDTH; i++ )); do
 
+    echo "i: $i, width: $WIDTH"
+
     for (( j=0; j<$HEIGHT; j++ )); do
         
         CLAIM_HITS=0
-        while read -r CLAIM && [[ $CLAIM_HITS -ne 2 ]]; do
-
-            # don't question the awk..... it works...
-            # source https://kuther.net/howtos/howto-print-text-between-tags-or-characters-awk-or-sed
-            CLAIM_X=$(echo $CLAIM | awk -F'[@ |,]' '{print $4}')
-            CLAIM_Y=$(echo $CLAIM | awk -F'[,|:]' '{print $2}')
-            CLAIM_W=$(echo $CLAIM | awk -F'[ |x]' '{print $4}')
-            CLAIM_H=$(echo $CLAIM | awk -F'[x|$]' '{print $2}')
-
-            #echo "($i,$j) $CLAIM --> $CLAIM_X $CLAIM_Y $CLAIM_W $CLAIM_H"
+        for (( k=0; k<$NUM_CLAIMS; k++ )); do
 
 
-            if [[ $i -ge $CLAIM_X ]] && 
-               [[ $j -ge $CLAIM_Y ]] && 
-               [[ $i -lt $(( $CLAIM_X + $CLAIM_W )) ]] &&
-               [[ $j -lt $(( $CLAIM_Y + $CLAIM_H )) ]]; then
+
+            # FIXME change to use map
+            if [[ $i -ge ${CLAIMS_MAP[$k,x]} ]] && 
+               [[ $j -ge ${CLAIMS_MAP[$k,y]} ]] && 
+               [[ $i -lt $(( ${CLAIMS_MAP[$k,x]} + ${CLAIMS_MAP[$k,w]} )) ]] &&
+               [[ $j -lt $(( ${CLAIMS_MAP[$k,y]} + ${CLAIMS_MAP[$k,h]} )) ]]; then
 
                 ((CLAIM_HITS++))
 
                 if [[ $CLAIM_HITS -eq 2 ]]; then
-                    echo "($i,$j) $CLAIM --> $CLAIM_X $CLAIM_Y $CLAIM_W $CLAIM_H [$CLAIM_HITS]"
+                    echo "($i,$j) $CLAIM --> ${CLAIMS_MAP[$k,x]} ${CLAIMS_MAP[$k,y]} ${CLAIMS_MAP[$k,y]} ${CLAIMS_MAP[$k,h]} [$CLAIM_HITS]"
+                    break
                 fi
 
             fi
 
 
-        done <<< "$CLAIMS"
+        done
 
         if [[ $CLAIM_HITS -eq 2 ]]; then
             ((OVERLAP_COUNT++))
