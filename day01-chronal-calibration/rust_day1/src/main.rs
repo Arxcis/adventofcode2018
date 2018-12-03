fe232c23933d8cb5cf6d9f7e84cc45d99d870cc5
Meltinglava <meltinglavaoutland@gmail.com>
2018-12-03 11:15:38 +0100

    cargo fmt and clippy fixes

--- day01-chronal-calibration/rust_day1/src/main.rs
+++ day01-chronal-calibration/rust_day1/src/main.rs
@@ -1,13 +1,13 @@
 use std::{
-    io::{self, prelude::*},
     collections::HashSet,
+    io::{self, prelude::*},
 };
 
-fn puzzle_1(state_changes: &Vec<i32>) -> i32 {
+fn puzzle_1(state_changes: &[i32]) -> i32 {
     state_changes.iter().sum()
 }
 
-fn puzzle_2(state_changes: &Vec<i32>) -> i32{
+fn puzzle_2(state_changes: &[i32]) -> i32 {
     let mut set = HashSet::new();
     let mut state = 0;
 
@@ -22,9 +22,10 @@ fn puzzle_2(state_changes: &Vec<i32>) -> i32{
 
 fn parse_file(reader: impl BufRead) -> std::io::Result<Vec<i32>> {
     //converts input in type of a file or stdin into an array of vec
-    Ok(reader.lines()
+    Ok(reader
+        .lines()
         .map(|l| {
-            let s = match l{
+            let s = match l {
                 Ok(n) => n,
                 Err(e) => {
                     eprintln!("Something wrong happend when loading the file:\n {:?}", e);
@@ -39,26 +40,23 @@ fn parse_file(reader: impl BufRead) -> std::io::Result<Vec<i32>> {
                 }
             }
         })
-       .collect())
+        .collect())
 }
 
-fn main() ->std::io::Result<()> {
+fn main() -> std::io::Result<()> {
     let stdin_input = io::stdin();
     let input = stdin_input.lock();
     let state_changes = parse_file(input)?;
     println!("{}", puzzle_1(&state_changes));
     println!("{}", puzzle_2(&state_changes));
-    
+
     Ok(())
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::{
-        fs::File,
-        io::BufReader,
-    };
+    use std::{fs::File, io::BufReader};
 
     fn tester(i: usize) -> (i32, Vec<i32>) {
         let file = File::open("../input").unwrap();
@@ -66,21 +64,21 @@ mod tests {
         let state_changes = parse_file(reader).unwrap();
         let output_file = File::open("../output").unwrap();;
         let ans = BufReader::new(output_file)
-                .lines()
-                .skip(i)
-                .next()
-                .unwrap()
-                .unwrap();
+            .lines()
+            .skip(i)
+            .next()
+            .unwrap()
+            .unwrap();
         let expected = i32::from_str_radix(&ans, 10).unwrap();
         (expected, state_changes)
     }
-        
+
     #[test]
     fn puzzle_tester_1() {
         let (expected, state_changes) = tester(0);
         assert_eq!(expected, puzzle_1(&state_changes));
     }
-    
+
     #[test]
     fn puzzle_tester_2() {
         let (expected, state_changes) = tester(1);
