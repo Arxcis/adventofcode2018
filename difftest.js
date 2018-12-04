6705afd7fa98f5b57383e58e0917a27a08727c33
Jonas Johan solsvik <jonasjso@protonmail.com>
2018-12-05 00:28:03 +0100

    difftest - set process exit code

--- difftest.js
+++ difftest.js
@@ -1,24 +1,24 @@
 const _exec = require('child_process').exec
 
 const main = async () => {
-    let diff = await exec('git diff --name-only master\n')
+    let diff = await exec('git diff --name-only master\n').catch(err => {process.exitCode = 1})
     let lines = diff.split('\n').filter(line => line)
 
     lines.forEach(async line => {
-        let match = line.match(/(day\d\d[^\/]*\/)main.([a-z]*)/)
+        let match = line.match(/(day\d\d[^\/]+\/)main.([a-z]+)/)
         if (!match){
             return;
         }
         let [_, day, language] = match
-        await exec(`npm run ${language} ${day}`)
+        await exec(`npm run ${language} ${day}`).catch(err => {process.exitCode = 1})
     })
 }
 
 const exec = commands => {
     return new Promise((resolve, reject) => {
-        console.log(commands)
+        //console.log(commands)
         _exec(commands, (err, stdout) => {
-            console.log(stdout)
+         //   console.log(stdout)
             if (err) {
                 reject(err)
             } else {
@@ -28,4 +28,4 @@ const exec = commands => {
     })
 }
 
-main().catch(err => {process.exit(1)})
+main().catch(err => {process.exitCode = 1})
