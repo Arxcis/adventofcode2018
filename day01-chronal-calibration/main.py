e5d4cf40f3f1eb3f7a20c8fc8d45f455b2562d8a
Jonas Johan solsvik <jonasjso@protonmail.com>
2018-12-04 22:40:33 +0100

    day01 main.py

---
+++ day01-chronal-calibration/main.py
@@ -0,0 +1,33 @@
+#!/usr/local/bin/python3
+
+import fileinput
+
+def main():
+    frequencyChanges = []
+    for line in fileinput.input():
+        frequencyChanges.append(line.replace("\n", ""))
+
+    #
+    # Part 1
+    #
+    currentFrequency = 0
+    for change in frequencyChanges:
+        currentFrequency += int(change)
+    
+    print(currentFrequency)
+
+    #
+    # Part 2
+    #
+    currentFrequency = 0
+    currentFrequencyVisited = {}
+
+    while True:
+        for change in frequencyChanges:
+            currentFrequency += int(change)
+            currentFrequencyVisited[currentFrequency] = 1
+
+
+
+if __name__ == "__main__":
+    main()
