93a1362991b1f6a38d159089f6181e39084821ed
tholok97 <thomas.lokkeborg@mimer.no>
2018-12-07 00:56:14 +0100

    shellchecked day05 bash

--- day05-alchemical-reduction/main.bash
+++ day05-alchemical-reduction/main.bash
@@ -9,19 +9,24 @@
 input=$(</dev/stdin)
 sed_pairs=$(echo {a..z} | sed 's/\(\b\w\b\)\s*/\1\u\1|\u\1\1|/g;s/|$//')
 part_1_solution=$(echo "$input" | sed -E ":a s/$sed_pairs//g;ta" | tr -d '\n' | wc -c)
-echo $part_1_solution
+echo "$part_1_solution"
 
 # PART 2
 
-min_char=ø
 min_length=$part_1_solution
 for char in {a..z}; do
-    completely_reacted=$(echo $input | sed "s/$char//Ig")
+
+    # remove all instances of $char
+    completely_reacted=${input//$char/}
+
+    # remove all instances of the uppercased $char
+    upperchar=${char^^}
+    completely_reacted=${completely_reacted//$upperchar/}
+
     length=$(echo "$completely_reacted" | sed -E ":a s/$sed_pairs//g;ta" | tr -d '\n' | wc -c)
     if [[ $length -lt $min_length ]]; then
         min_length=$length
-        min_char=$char
     fi
 done
 
-echo $min_length
+echo "$min_length"
