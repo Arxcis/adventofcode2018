b338e02a3d97c8c4d201e1e3554e0169a01100eb
tholok97 <thomas.lokkeborg@mimer.no>
2018-12-07 18:59:05 +0100

    ref #33: quoted some stuff - seems like osx bash is more strict about quotes - done to try and stop osx bash from outputing leading spaces

--- day05-alchemical-reduction/main.bash
+++ day05-alchemical-reduction/main.bash
@@ -6,9 +6,9 @@
 
 # PART 1
 
-input=$(</dev/stdin)
-sed_pairs=$(echo {a..z} | sed 's/\(\b\w\b\)\s*/\1\u\1|\u\1\1|/g;s/|$//')
-part_1_solution=$(echo "$input" | sed -E ":a s/$sed_pairs//g;ta" | tr -d '\n' | wc -c)
+input="$(</dev/stdin)"
+sed_pairs="$(echo {a..z} | sed 's/\(\b\w\b\)\s*/\1\u\1|\u\1\1|/g;s/|$//')"
+part_1_solution="$(echo "$input" | sed -E ":a s/$sed_pairs//g;ta" | tr -d '\n' | wc -c)"
 echo "$part_1_solution"
 
 # PART 2
