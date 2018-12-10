d546572bcac1fd3ecb113ec3d6467b203f43a63b
Jonas Johan solsvik <jonasjso@protonmail.com>
2018-12-10 21:22:58 +0100

    day04 python solution

--- day04-respose-record/main.py
+++ day04-respose-record/main.py
@@ -14,41 +14,13 @@ class Entry:
         self.command = command
         self.rest = rest
 
-class MinuteRange:
-    def __init__(self, start):
-        self.start = start;
-        self.stop = None
 
-class Shift:
-    
-    def __init__(self, guard_id):
+class Observation:
+    def __init__(self, guard_id, begin, end):
         self.guard_id = guard_id
-        self.minute_ranges = []
-        self.temp = None
+        self.begin = begin
+        self.end = end
 
-    def falls_asleep(self, start_minute):
-        self.temp = MinuteRange(start_minute)
-
-    def wakes_up(self, stop_minute):
-        self.temp.stop = stop_minute
-        self.minute_ranges.append(self.temp)
-        self.temp = None
-
-class MinuteCounter:
-    
-    def __init__(self):
-        self.minutes = [0 for i in range(0,59)]
-
-    def count(self, minute_ranges):
-        for minute_range in minute_ranges:
-            for i in range(minute_range.start, minute_range.stop):
-                self.minutes[i] += 1            
-
-    def sum(self):
-        return sum(self.minutes)
-
-    def __str__(self):
-        return f"{''.join((str(counter) for counter in self.minutes))}: {self.sum()}"
 
 def main():
 
@@ -60,61 +32,78 @@ def main():
     entries = [Entry(line) for line in fileinput.input()]
     entries.sort(key=lambda entry: entry.timestamp)
 
-
     #
     # ..parse each shift into Shift struct + keep track of unique guard ids
     #
-    guard_ids = {}
-    shifts = []
-    shift = None
     regex_guard = re.compile(r'#(\d+) begins shift')    
-   
+
+    observations = []
+    guard_id = None
+    begin = None
+    end = None
+    score = {}
+
     for entry in entries:
         if entry.command == 'Guard':
-            if shift is not None:
-                shifts.append(shift)
-                shift = None
-
-            [guard_id] = re.search(regex_guard, entry.rest).groups()
-            guard_ids[guard_id] = None
-            shift = Shift(guard_id)
-
-        elif entry.command == 'wakes':
-            shift.wakes_up(entry.minute)
+            [gid] = re.search(regex_guard, entry.rest).groups()
+            guard_id = int(gid)
+            score[int(guard_id)] = None
 
         elif entry.command == 'falls':
-            shift.falls_asleep(entry.minute)
+            begin = entry.minute
+
+        elif entry.command == 'wakes':
+            end = entry.minute
+            observations.append(Observation(guard_id, begin, end))
+
 
     #
-    # ...make one minute counter for each guard
+    # ...init score table
     #
-    minute_counters = {}
-    for guard_id in guard_ids.keys():
-        minute_counters[guard_id] = MinuteCounter()
+    for keys in score.keys():
+        score[keys] = [0 for i in range(0,59)]
+
 
     #
-    # ..count minutes of each guard
+    # ...count minutes
     #
-    for shift in shifts:
-        minute_counters[shift.guard_id].count(shift.minute_ranges)
+    for obs in observations:
+        for i in range(obs.begin, obs.end):
+            score[obs.guard_id][i] += 1
 
     #
-    # ...find the guard who sleeps most minutes
+    # ...sum minutes
     #
-    sums = zip(
-        [counter.sum() for counter in minute_counters.values()], 
-        minute_counters.values())
-
-    guard_who_sleeps_the_most = max([s for s in sums])
+    sums = {}
+    for guard_id, minutes in score.items():
+        sums[guard_id] = sum(minutes)
 
     #
-    # ...find the minute he sleeps the most
+    # ...max minutes for each guard
     #
-    minutes = enumerate(guard_who_sleeps_the_most[1].minutes)
-    minute_he_most_often_sleeps = max([(value, index) for index, value in minutes])
+
+    #
+    # ...find max guard and the minute he sleeps the most
+    #
+    maxguard = max([z for z in zip(sums.values(), sums.keys())])
+    maxminute = max([(val, i) for i, val in enumerate(score[maxguard[1]])])
+
+    guard_id_who_sleeps_the_most = maxguard[1]
+    minute_he_sleeps_the_most = maxminute[1]
     
-    print(guard_who_sleeps_the_most[0], minute_he_most_often_sleeps[1])
-    print(guard_who_sleeps_the_most[0] * minute_he_most_often_sleeps[1])
+    print(guard_id_who_sleeps_the_most * minute_he_sleeps_the_most)
+
+    #
+    # Part 2
+    #
+    maxminute = max((max(((minute, i, guard_id) 
+        for i, minute in enumerate(minutes))) 
+            for guard_id, minutes in score.items()))
+
+
+    print(maxminute[1] * maxminute[2])
+
+
 
 if __name__ == "__main__":
     main()
