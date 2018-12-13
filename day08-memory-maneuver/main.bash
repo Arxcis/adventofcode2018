901007006f6e58c0ee46de2c93acce59527d17a5
tholok97 <thomas.lokkeborg@mimer.no>
2018-12-13 19:40:34 +0100

    WIP solve for day08 bash part 1 - will clean up later. - uses recursion, is probably faster if make into loop

---
+++ day08-memory-maneuver/main.bash
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# read input as array
+input=($(</dev/stdin))
+
+function sum_metadata_for_node() {
+
+    local index=$1
+
+    local dbg=''
+
+    local sum=0
+
+    local num_children=${input[$index]}
+
+    ((index++))
+
+    local num_metadata=${input[$index]}
+
+    ((index++))
+
+    # declare counter i as local (idk how to do it in the for loop)
+    local i
+
+    # for every child call this function recursively to read it
+    for (( i=0; i<num_children; ++i )); do
+        child_ret=($(echo "$(sum_metadata_for_node $index)" | sed 's/(\([[:digit:]]*\)) (\([[:digit:]]*\))/\1 \2/'))
+        child_sum=${child_ret[0]}
+        child_index=${child_ret[1]}
+        sum=$(( child_sum + sum ))
+        index=$(( child_index ))
+        dbg+="[$index (added) $child_index]"
+    done
+
+
+
+    # add up metadata
+    last_metadata_index=$(( index + num_metadata ))
+    for (( index=index; index<last_metadata_index; ++index )); do
+        metadata="${input[$index]}"
+        sum=$(( sum + metadata ))
+    done
+
+    echo "($sum) ($index) $dbg"
+}
+
+echo "$(sum_metadata_for_node 0)"
+
+: << 'END_COMMENT'
+function readNode()
+    read num nodes
+    read num metadata
+    read each node
+        readNode()
+    read metadata
+END_COMMENT
