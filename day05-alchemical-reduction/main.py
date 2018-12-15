bd9ce16e9507988039f60d1a25e6f69fcc394760
Jonas Johan solsvik <jonasjso@protonmail.com>
2018-12-15 13:48:03 +0100

    day05-py - part 2

--- day05-alchemical-reduction/main.py
+++ day05-alchemical-reduction/main.py
@@ -2,23 +2,53 @@
 import fileinput
 import time
 import sys
-#
-# ------------- Part 1 ------------------
-#
-polymer = f"{[line for line in fileinput.input()][0]}"
+
 alphabet = "abcdefghijklmnopqrstuvwxyz"
+lower_UPPER_pairs = [(f"{c.lower()}{c.upper()}", 
+        f"{c.upper()}{c.lower()}") for c in alphabet]
 
-oldlen = 0
-newlen = len(polymer)
 
-while oldlen != newlen:
+def main():
+    global alphabet
+    initial_polymer = [line for line in fileinput.input()][0]
+    
+    #
+    # Part 1
+    #
+    print(fully_reacted_length(initial_polymer))
+
+    #
+    # Part 2
+    #
+    min_len = 10**100 # gogool
     for c in alphabet:
-        polymer = polymer.replace(f"{c}{c.upper()}", "")
-    for c in alphabet:
-        polymer = polymer.replace(f"{c.upper()}{c}", "")
+        new_len = fully_reacted_length(
+            initial_polymer
+            .replace(c.lower(), "")
+            .replace(c.upper(), ""))
 
-    oldlen = newlen
+        print(c, ":", new_len)
+        if new_len < min_len:
+            min_len = new_len
+
+    print(min_len)
+
+
+def fully_reacted_length(polymer):
+    global alphabet, lower_UPPER_pairs
+
+    oldlen = 0
     newlen = len(polymer)
-    print(oldlen, newlen)
 
-print(len(polymer))
+    while oldlen != newlen:
+        for lowerUPPER, UPPERlower in lower_UPPER_pairs:
+            polymer = polymer.replace(lowerUPPER, "")
+            polymer = polymer.replace(UPPERlower, "")
+
+        oldlen = newlen
+        newlen = len(polymer)
+
+    return len(polymer)
+
+if __name__ == "__main__":
+    main()
