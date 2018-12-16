1e0f4a009f1489f75aee9953d2d2ee3f81eb2877
Jonas Johan solsvik <jonasjso@protonmail.com>
2018-12-16 21:34:13 +0100

    test-util.js - only use the extension in language names, exclude main

--- test-util.js
+++ test-util.js
@@ -1,70 +1,76 @@
 const fs = require('fs')
 const _exec = require('child_process').exec
 
-const inputFile = 'input'
-const outputFile = 'output'
-const main_go = 'main.go'
-const main_bash = 'main.bash'
-const main_py = 'main.py'
-const main_cpp = 'main.cpp'
-const main_rs = 'main.rs'
-const main_js = 'main.js'
-
-const target_rs = 'main-rs'
+const input_file = 'input'
+const output_file = 'output'
 const target_cpp = 'main-cpp'
 
+const go = 'go';
+const bash = 'bash';
+const python = 'py';
+const cpp = 'cpp';
+const rust = 'rs';
+const node = 'js';
 
-exports.enabledFilenames = [
-    main_go,
-    main_py,
-    main_cpp,
-    main_rs,
-    main_js,
-    main_bash
+exports.enabledLanguages = [
+    go,
+    bash,
+    python,
+    cpp,
+    rust,
+    node
 ]
 
 //
 // Interpreted languages
 //
 /* Go 1.11 */
-exports[main_go] = makeTest(dirpath => exec(`cat ${dirpath}/${inputFile} | go run ${dirpath}/${main_go}`))
+exports[go] = makeTest(dirpath =>
+    exec(`cat ${dirpath}/${input_file} | go run ${dirpath}/main.${go}`))
 
 /* Bash 4.4 */
-exports[main_bash] = makeTest(dirpath => exec(`cat ${dirpath}/${inputFile} | bash ${dirpath}/${main_bash}`))
+exports[bash] = makeTest(dirpath =>
+    exec(`cat ${dirpath}/${input_file} | bash ${dirpath}/main.${bash}`))
 
 /* Python 3.6 */
-exports[main_py] = makeTest(dirpath => exec(`cat ${dirpath}/${inputFile} | python3 ${dirpath}/${main_py}`))
+exports[python] = makeTest(dirpath =>
+    exec(`cat ${dirpath}/${input_file} | python3 ${dirpath}/main.${python}`))
 
 /* Node 11 */
-exports[main_js] = makeTest(dirpath => exec(`cat ${dirpath}/${inputFile} | node ${dirpath}/${main_js}`))
+exports[node] = makeTest(dirpath =>
+    exec(`cat ${dirpath}/${input_file} | node ${dirpath}/main.${node}`))
 
 //
 // Compiled languages
 //
 /* C++ 17 */
-exports[main_cpp] = makeTest(async dirpath => {
+exports[cpp] = makeTest(async dirpath => {
 
-    await exec(`g++ -std=c++17 -O3 ${dirpath}/${main_cpp} -o ${dirpath}/${target_cpp}`)
-    let output = await exec(`cat ${dirpath}/${inputFile} | ${dirpath}/${target_cpp}`)
-    await exec(`rm ${dirpath}/${target_cpp}`)
+    await exec(`g++ -std=c++17 -O3 ${dirpath}/main.${cpp} -o ${dirpath}/main-${cpp}`)
+    let output = await exec(`cat ${dirpath}/${input_file} | ${dirpath}/main-${cpp}`)
+    await exec(`rm ${dirpath}/main-${cpp}`)
 
     return output;
 })
 
 /* Rust */
-exports[main_rs] = makeTest(async dirpath => {
+exports[rust] = makeTest(async dirpath => {
 
-    await exec(`rustc ${dirpath}/${main_rs} -o ${dirpath}/${target_rs} -C debuginfo=0 -C opt-level=3`);
-    let output = await exec(`cat ${dirpath}/${inputFile} | ${dirpath}/${target_rs}`)
-    await exec(`rm ${dirpath}/${target_rs}`)
+    let output = await exec(`\
+cat ${dirpath}/input |\
+cargo run \
+--manifest-path ${dirpath}/main.${rust}/Cargo.toml \
+--release \
+--quiet`);
 
+    await exec(`cargo clean --manifest-path ${dirpath}/main.${rust}/Cargo.toml`)
     return output
 })
 
 function makeTest(producer) {
     return async (t, dirpath) => {
         try { 
-            var expected = await readFile(`${dirpath}/${outputFile}`)
+            var expected = await readFile(`${dirpath}/${output_file}`)
             expected = expected.split('\n').filter(expected => expected)
         } catch (e) { 
             t.fail(e) 
