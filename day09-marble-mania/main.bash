256f02b220aa7539588d21b7c7950a907b967569
tholok97 <thomas.lokkeborg@mimer.no>
2018-12-16 20:25:48 +0100

    WIP solution for day9 bash part 1 - main.bash contains solution with array, main2.bash contains solution with doubly linked list - solution in main2.bash solves in about a minute, main.bash is wayyy too slow - will clean up and make main2.bash the actual solution

--- day09-marble-mania/main.bash
+++ day09-marble-mania/main.bash
@@ -1,46 +1,126 @@
 #!/bin/bash
 
+
+
+
+
+
+
+
+
 # https://unix.stackexchange.com/questions/328882/how-to-add-remove-an-element-to-from-the-array-in-bash
 
-array=(0)
-current=0
-marble=0
+input=$(</dev/stdin)
+parsed_input=($(echo "$input" | sed 's/^\([[:digit:]]*\) players; last marble is worth \([[:digit:]]*\) points$/\1 \2/'))
 
-function inc_current() {
-    ((current++))
-    if [[ $current -ge "${#array[@]}" ]]; then
-        current=0
+
+num_players=${parsed_input[0]}
+last_marble=${parsed_input[1]}
+
+echo "${parsed_input[@]}"
+
+
+array=(0)
+current_index=0
+current_player=1
+marble=1
+
+function inc_current_index() {
+    ((current_index++))
+    if [[ $current_index -ge "${#array[@]}" ]]; then
+        current_index=0
     fi
 }
 
-while true; do
+function inc_current_player() {
+    ((current_player++))
+    if [[ $current_player -gt "$num_players" ]]; then
+        current_player=1
+    fi
+}
 
-    ((marble++))
-    echo -e "->$current ($marble) \t${array[@]}"
+
+declare -A player_map
+
+while [[ $marble -le $last_marble ]]; do
+
+
 
     if (( !(marble % 23) )); then
     
-        current=$(( current - 8 ))
-        echo "$marble yay! (removed $current which was ${array[$current]})"
-        
+        echo "yay index is $current_index tho (-7: $(( current_index - 7 )))"
+        current_index=$(( current_index - 7 ))
 
-        # remove element at current by shifting array to the left after current
-        length=${#array[@]}
-        for (( i = current; i < length; ++i )); do
-            array[$i]="${array[$(( i + 1 ))]}"
+        # (never happens??)
+        if [[ $current_index -lt 0 ]]; then
+            echo "array: ${array[@]}"
+            length=${#array[@]}
+            echo "OBS was $current_index (length is $length) (math: $(( current_index + length ))"
+            current_index=$(( current_index + length ))
+            echo "OBS $current_index"
+        fi
+
+        echo "$marble yay! (removed $current_index which was ${array[$current_index]})"
+
+        # give points to current_index player
+        previous_score="${player_map[$current_player]}"
+        removed_marble="${array[$current_index]}"
+        player_map[$current_player]=$(( previous_score + marble + removed_marble))
+        echo "player $current_player now has ${player_map[$current_player]} score [$previous_score + $marble + $removed_marble]"
+
+        # remove element at current_index by shifting array to the left after current_index
+        #length=${#array[@]}
+        #for (( i = current_index; i < length; ++i )); do
+        #    array[$i]="${array[$(( i + 1 ))]}"
+        #done
+        #length_minus_one=$(( length - 1 ))
+        #unset array[$length_minus_one]
+
+        #array=( "${array[@]:0:$(( current_index - 1 ))}" "${array[@]:$(( current_index ))}")
+
+        unset array[$current_index]
+        new_array=()
+        for value in ${array[@]}; do
+            new_array+=($value)
         done
-        unset "array[$length]"
+        array=()
+        for value in ${new_array[@]}; do
+            array+=($value)
+        done
 
-        inc_current
+        #echo "val: ${array[$current_index]}"
+
 
     else
 
-        inc_current
-        array=( "${array[@]:0:$current}" "$marble" "${array[@]:$current}" )
-        inc_current
+        inc_current_index
+        inc_current_index
+        #echo "-->$current_index"
+        array=( "${array[@]:0:$current_index}" "$marble" "${array[@]:$current_index}" )
+        
+        # make space
+        #length="${#array[@]}"
+        #for (( i = current_index + 1; i <= length; ++i )); do
+        #    array[$i]=${array[$(( i - 1 ))]}
+        #done
+        #array[$current_index]=$marble
 
     fi
 
-    sleep 0.1
+    #echo -e "[$current_player]->$current_index ($marble) \t${array[@]}"
+
+    inc_current_player
+    ((marble++))
+
 
 done
+
+# find winner
+max_score=-1
+for score in "${player_map[@]}"; do
+    if [[ $score -gt $max_score ]]; then
+        max_score=$score
+    fi
+done
+
+echo "$max_score"
---
+++ day09-marble-mania/main2.bash
@@ -0,0 +1,171 @@
+#!/bin/bash
+
+
+
+function inset_after_current() {
+
+    after=$current_id
+    value=$1
+
+    map_v[$next_id]=$value
+
+    map_p[${map_n[$after]}]=$next_id
+
+    map_n[$next_id]=${map_n[$after]}
+    map_p[$next_id]=$after
+
+    map_n[$after]=$next_id
+
+    ((next_id++))
+}
+
+function delete_current_and_set_to_next() {
+
+    current_p=${map_p[$current_id]}
+    current_n=${map_n[$current_id]}
+    
+    map_n[$current_p]=$current_n
+    map_p[$current_n]=$current_p
+
+    unset map[$current_id]
+
+    current_id=$current_n
+}
+
+function clockwise() {
+    current_id="${map_n[$current_id]}"
+}
+
+function counter_clockwise() {
+    current_id="${map_p[$current_id]}"
+}
+
+function print_current_verbose() {
+    echo -e "[${map_v[$current_id]}]\t${map_p[$current_id]} <- $current_id -> ${map_n[$current_id]}"
+}
+
+function print_current() {
+    echo "${map_v[$current_id]}"
+}
+
+function print_list_verbose() {
+    local backup_current=$current_id
+
+    echo "LIST:"
+    print_current_verbose
+    clockwise
+    while [[ $current_id -ne $backup_current ]]; do
+
+        print_current_verbose
+        clockwise
+
+    done
+
+    current_id=$backup_current
+}
+
+function print_list() {
+    local backup_current=$current_id
+    current_id=0
+
+    printf "LIST: "
+    printf "$(print_current) "
+    clockwise
+    while [[ $current_id -ne "0" ]]; do
+
+        printf "$(print_current) "
+
+        clockwise
+
+    done
+    echo;
+
+    current_id=$backup_current
+}
+
+declare -A map_n
+declare -A map_p
+
+
+input=$(</dev/stdin)
+parsed_input=($(echo "$input" | sed 's/^\([[:digit:]]*\) players; last marble is worth \([[:digit:]]*\) points$/\1 \2/'))
+
+
+num_players=${parsed_input[0]}
+last_marble=${parsed_input[1]}
+
+map_v[0]=0
+map_n[0]=0
+map_p[0]=0
+
+next_id=1
+current_id=0
+
+marble=1
+
+
+current_player=1
+
+
+declare -A player_map
+
+while [[ $marble -le $last_marble ]]; do
+
+
+
+    if (( !(marble % 23) )); then
+
+        counter_clockwise
+        counter_clockwise
+        counter_clockwise
+        counter_clockwise
+        counter_clockwise
+        counter_clockwise
+        counter_clockwise
+
+
+        previous_score="${player_map[$current_player]}"
+        deleted_marble=$(print_current)
+
+
+        player_map[$current_player]=$(( previous_score + marble + deleted_marble ))
+
+        echo "(player: $current_player)one to be deleted is $(print_current). score is $marble + $previous_score + $deleted_marble = ${player_map[$current_player]}"
+
+        delete_current_and_set_to_next
+
+    else
+
+
+        clockwise
+        inset_after_current $marble
+        clockwise
+
+
+    fi
+
+    #echo "marble: $marble, player: $current_player-----------------------------"
+    #print_list_verbose 
+    #printf "[player: $current_player] "
+    #print_list
+
+    ((marble++))
+
+    ((current_player++))
+    if [[ $current_player -gt "$num_players" ]]; then
+        current_player=1
+    fi
+
+done
+
+
+# find winner
+max_score=-1
+for score in "${player_map[@]}"; do
+    echo "$score"
+    if [[ $score -gt $max_score ]]; then
+        max_score=$score
+    fi
+done
+
+echo "$max_score"
