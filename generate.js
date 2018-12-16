7b79b5b1d977651dd94174ce75c1373d093c3ef5
Jonas Johan solsvik <jonasjso@protonmail.com>
2018-12-16 21:02:41 +0100

    generate.js - only generate tests for folders which contain testable programs

--- generate.js
+++ generate.js
@@ -13,22 +13,28 @@ let dayDirs = files.filter(file => file.isDirectory())
 
 dayDirs.forEach(dir => {
     let abspath = `${__dirname}/${dir.name}`
-
     let dayFiles = fs.readdirSync(abspath, { withFileTypes: true })
-    let mainFiles = dayFiles.filter(file => file.isFile())
+
+    // .. If no testable file for a given day, dont generate tests
+    if ((!dayFiles.some(file =>
+            enabledFilenames.some(name =>
+                name === file.name)))) {
+        return;
+    }
+
+    let testableFiles = dayFiles.filter(file =>
+        enabledFilenames.some(name => name === file.name))
 
     let testFile = `\
 import test from 'ava'
 import testUtil from '../test-util'
 const dirpath = __dirname
 
-${mainFiles.reduce((str, main) => {
-
-    if (enabledFilenames.some(name => name === main.name)) {
-        str += `test('${main.name}', async t => await testUtil['${main.name}'](t, dirpath))\n`
-    }
+${testableFiles.reduce((str, file) => {
+    str += `test('${file.name}', async t => await testUtil['${file.name}'](t, dirpath))\n`
     return str
 }, "")}`
 
-    fs.writeFile(`${abspath}/test.js`, testFile, () => console.log(`Generated ${dir.name}/test.js`))
+    fs.writeFile(`${abspath}/test.js`, testFile, () =>
+        console.log(`Generated ${dir.name}/test.js`))
 })
