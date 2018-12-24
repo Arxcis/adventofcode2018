5c291e68a57018d1136f011b616dbdf3c98d2475
Jonas Johan solsvik <jonasjso@protonmail.com>
2018-12-24 13:52:19 +0100

    day08-cpp - part 2 complete

--- day08-memory-maneuver/main.cpp
+++ day08-memory-maneuver/main.cpp
@@ -7,7 +7,6 @@ struct Node {
     using id = int;
     std::vector<id> children;
     std::vector<int> metadata;
-    int value = 0;
 };
 
  Node make_node(std::vector<Node>& nodes, std::stringstream& ss) {
@@ -19,8 +18,8 @@ struct Node {
     ss >> metadata_count;
 
     for (int i = 0; i < child_count; ++i) {
-        node.children.emplace_back(nodes.size());
         nodes.emplace_back(make_node(nodes, ss));
+        node.children.emplace_back(nodes.size() - 1);
     }
 
     for (int i = 0; i < metadata_count; ++i) {
@@ -30,6 +29,29 @@ struct Node {
     }
     return node;
 };
+
+int value_of(const Node& node, const std::vector<Node>& nodes) {
+
+    int value = 0;
+
+    // ... if no children
+    if (node.children.size() == 0) {
+        for (int i = 0; i < node.metadata.size(); ++i) {
+            value += node.metadata[i];
+        } 
+    } else { // ... if has children
+        for (int i = 0; i < node.metadata.size(); ++i) {
+            // ... if metadata index within children index range
+            if (0 < node.metadata[i] && node.metadata[i] <= node.children.size()) {
+                int childIndex = node.metadata[i]-1;
+                int nodeIndex = node.children[childIndex];
+                auto child = nodes[nodeIndex];
+                value += value_of(child, nodes);
+            }
+        }
+    }
+    return value;
+}
 
 int main() {
 
@@ -44,8 +66,9 @@ int main() {
 
     //
     // Part 1: 
-    //  ... parse nodes
-    //  ... sum all metadata on each node
+    //
+    // ...parse nodes
+    // ...sum all metadata on each node
     //
     auto nodes = std::vector<Node>{};
     std::stringstream ss(input);
@@ -62,4 +85,9 @@ int main() {
     //
     // Part 2:
     //
+    // ...find value of the root node
+    auto root = nodes[nodes.size()-1];
+    int value_of_root_node = value_of(root, nodes);
+    printf("%d\n", value_of_root_node);
 }
+    
